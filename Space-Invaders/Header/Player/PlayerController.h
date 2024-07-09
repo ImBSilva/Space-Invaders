9e3598baf6fbd85367a6515af1459d2ee3c60b6f
@ImBSilva <brunoreinaldosilva@gmail.com>
2024-07-09 04:15:02 -0300

    inplement namespaces

--- Space-Invaders/Header/Player/PlayerController.h
+++ Space-Invaders/Header/Player/PlayerController.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 
+<<<<<<< HEAD
 enum class PlayerState;
 class PlayerView;
 class PlayerModel;
@@ -26,3 +27,36 @@ public:
     sf::Vector2f getPlayerPosition();
 
 };
+=======
+namespace Player {
+
+    enum class PlayerState;
+    class PlayerView;
+    class PlayerModel;
+
+    class PlayerController {
+
+    private:
+        PlayerModel* player_model;
+        PlayerView* player_view;
+
+        void processPlayerInput();
+        void moveLeft();
+        void moveRight();
+
+    public:
+        PlayerController();
+        ~PlayerController();
+
+        void initialize();
+        void update();
+        void render();
+
+        sf::Vector2f getPlayerPosition();
+        int getPlayerScore();
+        PlayerState getPlayerState();
+
+    };
+
+}
+>>>>>>> parent of ea33e57 (Revert "inplement namespaces")
--- Space-Invaders/Source/Player/PlayerService.cpp
+++ Space-Invaders/Source/Player/PlayerService.cpp
@@ -1,6 +1,7 @@
 #include "../../header/player/PlayerService.h"
 #include "../../header/Player/PlayerController.h"
 
+<<<<<<< HEAD
 PlayerService::PlayerService()
 {
 	player_controller = new PlayerController();
@@ -25,3 +26,33 @@ void PlayerService::render()
 {
 	player_controller->render();
 }
+=======
+namespace Player {
+
+	PlayerService::PlayerService()
+	{
+		player_controller = new PlayerController();
+	}
+
+	PlayerService::~PlayerService()
+	{
+		delete (player_controller);
+	}
+
+	void PlayerService::initialize()
+	{
+		player_controller->initialize();
+	}
+
+	void PlayerService::update()
+	{
+		player_controller->update();
+	}
+
+	void PlayerService::render()
+	{
+		player_controller->render();
+	}
+
+}
+>>>>>>> parent of ea33e57 (Revert "inplement namespaces")
